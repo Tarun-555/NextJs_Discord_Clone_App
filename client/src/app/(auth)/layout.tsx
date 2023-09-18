cf0123dc3786d965bef4dc37a134e6355f4c6aab
Tarun <trn18r@gmail.com>
2023-09-18 19:41:08 +0530

    Initial nextjs project setup with nestjs as backend

---
+++ client/src/app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+    return <div className='flex items-center justify-center h-screen bg-slate-900'>{children}</div>;
+};
+
+export default AuthLayout;
